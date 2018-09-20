4c829fd7c4485aea6e6493677f891fb8921e3eb1
Zarema <zarema.khalilova@gmail.com>
2018-09-20 20:00:54 +0300

    Add storybook and babel config

---
+++ src/components/Layout.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Layout = () => (
+  <div>I‘m layout</div>
+)
+
+export default Layout
---
+++ stories/Layout.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {storiesOf} from '@storybook/react'
+import Layout from '../src/components/Layout'
+
+storiesOf('components/Layout', module)
+  .add('default', () => (
+    <Layout />
+  ))
